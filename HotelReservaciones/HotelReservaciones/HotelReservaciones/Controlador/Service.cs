ccf1a0edf45b26a4e5cc15fa0b6f6bb2211e8d71
davidnavarrete <davidnava1998efrain@outlook.com>
2023-01-20 21:30:49 -0500

    registrar nuevo hotel
    registro de nuevo hotel

---
+++ HotelReservaciones/HotelReservaciones/HotelReservaciones/Controlador/Service.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace HotelReservaciones.Controlador
+{
+	public class Service
+	{
+		
+
+		public string urlHotel()
+		{
+			string url = "http://192.168.100.50/AppHotelService/Hotel.php";
+			return url;
+
+        }
+       
+	}
+}
+
--- HotelReservaciones/HotelReservaciones/HotelReservaciones/Datos/Hotel.cs
+++ HotelReservaciones/HotelReservaciones/HotelReservaciones/Datos/Hotel.cs
@@ -3,9 +3,14 @@ namespace HotelReservaciones.Datos
 {
 	public class Hotel
 	{
-		public Hotel()
-		{
-		}
-	}
+        public int idHotel { get; set; }
+        public string nombreHotel { get; set; }
+        public string descripcionHotel { get; set; }
+        public string correoHotel { get; set; }
+        public string direccionHotel { get; set; }
+        public string ubicacionHotel { get; set; }
+        public string telefonoHotel { get; set; }
+        public string estadoHotel { get; set; }
+    }
 }
 
